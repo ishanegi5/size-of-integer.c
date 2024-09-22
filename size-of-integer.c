9d89598fff7cf2d2392644801fa76407e23a4b9c
Isha Negi <itsishanegi@gmail.com>
2024-09-22 10:38:06 +0530

    Add files via upload

---
+++ size-of-integer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+
+{ int a[]={67,454,333,86,87,122,75,232,655,85,233,673132,76,6,8,3};
+
+int length = sizeof(a)/sizeof(a[0]);
+
+printf("length of a= %d",length);
+  
+return 0;  
+
+}
